fcddb33ce389e1daee217d67ab667ba40427a8a0
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-06-26 17:12:27 -0600

    helpers: separate functions

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,7 +1,2 @@
 export { formatDate } from './formatDate';
-
-export const generateUniqueId = () => {
-  const random = Math.random().toString(36).substring(2);
-  const currentDate = Date.now().toString(36);
-  return random + currentDate;
-}
+export { generateUniqueId } from './generateUniqueId';
