58fe361dece8a1c76d81ac679f7a67d7b9ab7228
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-06-28 12:22:39 -0600

    Add: react-circular-progressbar functionality

--- src/components/ControlBudget.jsx
+++ src/components/ControlBudget.jsx
@@ -1,23 +1,39 @@
 import { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles  } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 import { formatToDollars } from "../helpers";
 
 const ControlBudget = ({ budget, expenses }) => {
 
+  const [ percent, setPercent ] = useState(0);
   const [ available, setAvailable ] = useState(0);
   const [ spent, setSpent ] = useState(0);
 
   useEffect( () => {
     const totalSpent = expenses.reduce( ( total, expense ) => total + expense.qty, 0 );
     const totalAvailable = budget - totalSpent;
-
+    const porcentUpdated = ( ( (budget - totalAvailable) / budget ) * 100 ).toFixed(1);
+  
     setSpent(totalSpent);
     setAvailable(totalAvailable);
+    setTimeout(() => {
+      setPercent(porcentUpdated);
+    }, 1000);
   }, [ expenses ])
 
   return (
     <div className="container-budget container shadow two-columns">
       <div>
-        <p>Gráfica aquí</p>
+        <CircularProgressbar
+          styles={buildStyles({
+            pathColor: '#3B82F6',
+            trailColor: '#F5F5F5',
+            textColor: '#3B82F6',
+          })} 
+          value={percent}
+          text={`${percent}% Gastado`}
+        />
       </div>
 
       <div className="content-budget">
